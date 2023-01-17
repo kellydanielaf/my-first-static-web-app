50ba4a19154cc47effb955f79102107416f72c3f
kellydanielaf <122519886+kellydanielaf@users.noreply.github.com>
2023-01-17 11:57:46 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return (<><div>Hello the {value}</div><div><button title="Press me" color="#f194ff" onPress={() => Alert.alert('Button with adjusted color pressed')}/></div>)</>;
+  return (<><div>Hello the {value}</div><div><button title="Press me" color="#f194ff" onPress={() => Alert.alert('Button with adjusted color pressed')}/></div></>);
 }
 
 export default App;
