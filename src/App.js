094fbda9a621b54c13113d2ab2823cc48f48d4a4
kellydanielaf <122519886+kellydanielaf@users.noreply.github.com>
2023-01-17 11:31:10 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}</div>;
+  return <div><button>Merge</button></div>;
+  /*return (
+    <div>
+      <button>Merge</button>
+      </div>                       
+  );*/
 }
 
 export default App;
