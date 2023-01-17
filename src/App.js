327af21ec512c33387c6e80d4e9ea16507b0b0c0
kellydanielaf <122519886+kellydanielaf@users.noreply.github.com>
2023-01-17 14:40:14 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ import { Button, Alert } from 'react-native';
 
 function App() {
   const value = 'World';
-  return (<><div>Hello the {value}</div><div><button title="Press me" color="#f194ff" onPress={() => Alert.alert('Button with adjusted color pressed')}/></div></>);
+  return (<>
+            <div>Hello the {value}</div>
+            <div>
+              <button title="Press me" color="#f194ff" onclick="myFunction()">Button <\button>
+                <script>
+                  function myFunction() {
+                    alert("Your file is being uploaded!")
+                  }
+                </script>
+            </div>
+          </>);
 }
 
 export default App;
