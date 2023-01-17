ba7852ac6d893355b4a7e10df3b8b3b782ca0bb9
kellydanielaf <122519886+kellydanielaf@users.noreply.github.com>
2023-01-17 15:25:50 +0100

    ajout du context

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import cma from "../api/management_api";
+import { AppContext } from "../context";
 
 function App() {
   const value = 'World';
+  const { app } = useContext(AppContext);
+
+
+  const onMerge = async () => {
+    const ra = await cma.createContentType(app.config.api_key, app.config.management_token, app.config.endpoint, type.src, destinationBranch);
+  }
+
+
   return (<>
             <div>Hello the {value}</div>
             <div>
-              <button title="Press me" color="#f194ff" onclick="myFunction()">Button </button>
+              <button title="Press me" color="#f194ff" onclick="myFunction()">Button</button>
                 <script>
                   function myFunction() {
                     alert("Your file is being uploaded!")
