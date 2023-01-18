0cba4063c5e58726e9125064711b7a992e8d77fc
kellydanielaf <122519886+kellydanielaf@users.noreply.github.com>
2023-01-18 14:26:07 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   const value = 'World';
   const { app } = useContext(AppContext);
 
+  
 
   const onMerge = async () => {
     const ra = await cma.createEntry("link_component", "{"title":"link6","global_field":{"type":"link","data":{"link":{"link":{"title":"title","href":"url"},"uri":"","text":"","eventvalue":"ev","arialabel":"","select":"self"}}},"tags":[],"locale":"fr","uid":"blt4e1f8bfa37dc4aff","created_by":"blt29214e078f20019d","updated_by":"blt29214e078f20019d","created_at":"2022-11-22T13:21:07.494Z","updated_at":"2022-11-22T13:21:07.494Z","ACL":{},"_version":1,"_in_progress":false,"publish_details":[{"environment":"bltb96fb5bff0400f58","locale":"fr","time":"2022-11-22T13:21:09.816Z","user":"blt29214e078f20019d","version":1}]}");
